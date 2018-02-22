8075e8900254f0b722b9674c7ccb7260b0935248
Anton Bergman <antonbergman_@hotmail.com>
2018-02-22 21:17:16 +0100

    Removed comments

--- Assets/Scripts/BossLookAt.cs
+++ Assets/Scripts/BossLookAt.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class BossLookAt : MonoBehaviour {
 
 	void Update () {
-		// if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Moved){
-		// 	Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-		// 	transform.rotation = Quaternion.LookRotation(Vector3.forward, touchPos - transform.position);
-		// }
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Moved){
+			Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+			transform.rotation = Quaternion.LookRotation(Vector3.forward, touchPos - transform.position);
+		}
 	}
 }
