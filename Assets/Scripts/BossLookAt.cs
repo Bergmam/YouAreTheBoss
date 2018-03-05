63ef71c7ef38a891dbf948fd4b751d5e455eb8b2
leigh <lagebergman@gmail.com>
2018-03-05 20:14:29 +0100

    Prevent boss from rotating back to default on mobile devices

--- Assets/Scripts/BossLookAt.cs
+++ Assets/Scripts/BossLookAt.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class BossLookAt : MonoBehaviour {
 
-	void Update () {
+	void Update ()
+	{
 		Vector3 touchPos = Vector3.zero;
-		if (Input.touchCount > 0){
+		if (Input.touchCount > 0)
+		{
 			if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Moved){
 				touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 			}
-		} else {
+		}
+		else
+		{
 			touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		}
-		if (touchPos.y > Parameters.ARENA_BOTTOM) {
+		if (touchPos.y > Parameters.ARENA_BOTTOM && !touchPos.Equals (Vector3.zero))
+		{
 			transform.rotation = Quaternion.LookRotation (Vector3.forward, touchPos - transform.position);
 		}
 	}
