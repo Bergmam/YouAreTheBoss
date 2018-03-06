622c0e079b883ed67a8ab48b019b0fc9859b7bd0
leigh <lagebergman@gmail.com>
2018-03-06 10:30:24 +0100

    Change from spawning random enemies to spawning units from waves

---
+++ Assets/Scripts/Data/StatsHolder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class StatsHolder {
+	public string Name;
+	public float MovementSpeed;
+	public float Damage;
+	public float Range;
+	public float Health;
+	public float Scale;
+	public Color Color;
+
+	public StatsHolder(string name, 
+		float MovementSpeed, 
+		float Damage, 
+		float Range, 
+		float Health, 
+		float Scale,
+		Color Color) {
+		this.Name = name;
+		this.MovementSpeed = MovementSpeed;
+		this.Damage = Damage;
+		this.Range = Range;
+		this.Health = Health;
+		this.Scale = Scale;
+		this.Color = Color;
+	}
+}
---
+++ Assets/Scripts/Data/SubWave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+
+public class SubWave {
+
+	private List<StatsHolder> enemies;
+	private float duration;
+
+	public SubWave(List<StatsHolder> enemies, float duration){
+		this.enemies = enemies;
+		this.duration = duration;
+	}
+
+	public List<StatsHolder> GetEnemies(){
+		return this.enemies;
+	}
+
+	public float GetDuration(){
+		return this.duration;
+	}
+
+}
