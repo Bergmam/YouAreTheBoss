ba7e736082e1c8b98d85c22eee977a8179f4f6b7
Anton Bergman <antonbergman_@hotmail.com>
2018-03-07 19:12:17 +0100

    Add wave information text in main menu

--- Assets/Scripts/Data/WaveFactory.cs
+++ Assets/Scripts/Data/WaveFactory.cs
@@ -15,18 +15,18 @@ public class WaveFactory
 		while (n >= 0) {
 			
 			if (n % 10 == 0 && n > 0) { // Every 10th wave: Add a subwave containing a slow enemy.
-				SubWave subWave = new SubWave (0.0f);
-				subWave.AddEnemy (new StatsHolder ("SlowEnemy", 0.3f, 6.0f, 1.0f, 300.0f, 2.0f, Color.black));
+				SubWave subWave = new SubWave (0.5f);
+				subWave.AddEnemy (new StatsHolder ("Slow Enemy", 0.3f, 6.0f, 1.0f, 300.0f, 2.0f, Color.black));
 				wave.Add (subWave);
 				n -= 10;
 			} else if (n % 2 == 0 && n > 0) { // Every 2nd wave: Add a subwave containing a fast enemy.
-				SubWave subWave = new SubWave (1.0f);
-				subWave.AddEnemy (new StatsHolder ("FastEnemy", 3.0f, 1.0f, 2.0f, 50.0f, 0.5f, Color.yellow));
+				SubWave subWave = new SubWave (0.5f);
+				subWave.AddEnemy (new StatsHolder ("Fast Enemy", 3.0f, 1.0f, 2.0f, 50.0f, 0.5f, Color.yellow));
 				wave.Add (subWave);
 				n -= 2;
 			} else {
-				SubWave subWave = new SubWave (4.0f);  // 
-				subWave.AddEnemy (new StatsHolder ("StandardEnemy", 1.0f, 2.5f, 1.0f, 100.0f, 1.0f, Color.white));
+				SubWave subWave = new SubWave (0.5f);  // 
+				subWave.AddEnemy (new StatsHolder ("Standard Enemy", 1.0f, 2.5f, 1.0f, 100.0f, 1.0f, Color.white));
 				wave.Add (subWave);
 				n--;
 			}
---
+++ Assets/Scripts/Wave/WaveUnitsText.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveUnitsText : MonoBehaviour {
+
+	// Use this for initialization
+	Dictionary<string, int> enemyTypeToAmount = new Dictionary<string, int>();
+	void Start () {
+
+		Text text = GetComponent<Text>();
+
+		foreach(SubWave subWave in WaveFactory.GenerateWave(WaveNumber.waveNumber)){
+			foreach(StatsHolder statsHolder in subWave.GetEnemies()){
+				if (!enemyTypeToAmount.ContainsKey(statsHolder.Name)){
+					enemyTypeToAmount.Add(statsHolder.Name, 1);
+				} else {
+					enemyTypeToAmount[statsHolder.Name]++;// enemyTypeToAmount[statsHolder.Name] + 1);
+				}
+			}
+		}
+
+		string enemyInfoText = "";
+		List<string> nameList = new List<string>();
+
+		foreach(KeyValuePair<string, int> entry in enemyTypeToAmount){
+			nameList.Add(entry.Key + ": " + entry.Value + "\n");
+		}
+
+		nameList.Sort();
+		foreach(string name in nameList){
+			enemyInfoText += name;
+		}
+		text.text = enemyInfoText.ToUpper();
+		
+	}
+}
