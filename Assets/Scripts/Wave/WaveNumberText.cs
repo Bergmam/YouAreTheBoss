7a0cc8175a04221c773deecfa22a069ad2c04c8a
Anton Bergman <antonbergman_@hotmail.com>
2018-03-07 18:40:05 +0100

    Add score labels in both scenes

---
+++ Assets/Scripts/Wave/WaveNumberText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveNumberText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Text text = GetComponent<Text>();
+		text.text = "SCORE: " + WaveNumber.waveNumber;
+	}
+}
