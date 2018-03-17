ba973e50e32d053e3574a61e7376b48e4bf4360c
leigh <lagebergman@gmail.com>
2018-03-17 13:46:26 +0100

    Create new class RadialPosition

---
+++ Assets/Scripts/Data/RadialPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialPosition {
+
+	private float radius;
+	private float angle;
+
+	public RadialPosition (float radius, float angle) {
+		this.radius = radius;
+		this.angle = angle;
+	}
+
+	public float GetRadius() {
+		return this.radius;
+	}
+
+	public float GetAngle () {
+		return this.angle;
+	}
+
+}
