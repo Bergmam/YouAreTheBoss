dd8f70b9ac7174b1c894a984ef62f1b12b8b332a
leigh <lagebergman@gmail.com>
2018-04-02 16:57:09 +0200

    Make InCounterClockwiseLimits treat very small angles as equal

--- Assets/Scripts/Utils/RotationUtils.cs
+++ Assets/Scripts/Utils/RotationUtils.cs
@@ -24,7 +24,11 @@ public class RotationUtils
 		bool zeroInLimits = high < low;
 		bool inLimitsAroundZero = zeroInLimits && (angle < high || angle > low);
 		bool inLimitsWithoutZero = !zeroInLimits && (angle > low && angle < high);
-		return inLimitsAroundZero || inLimitsWithoutZero || low == high;
+        bool lowEqualsHigh = Mathf.Abs(high-low) < 0.05f;
+        if(Mathf.Abs(high-low) > 0.05f && Mathf.Abs(high-low) < 5f){
+            throw new System.Exception("An angle of size " + Mathf.Abs(high-low) + " was found!");
+        }
+		return inLimitsAroundZero || inLimitsWithoutZero || lowEqualsHigh;
 	}	
 
 	/// <summary>
