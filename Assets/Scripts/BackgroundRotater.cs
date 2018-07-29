49bd64ea198f2ab77f4a8b688ad4d01b587beaaa
Anton Bergman <antonbergman_@hotmail.com>
2018-07-29 14:41:37 +0200

    Add background graphics to fight scene

---
+++ Assets/Scripts/BackgroundRotater.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundRotater : MonoBehaviour {
+
+	GameObject rotator1;
+	GameObject rotator2;
+	GameObject rotator3;
+
+	List<GameObject> rotators = new List<GameObject>();
+	Dictionary<string, float> rotatorSpeeds = new Dictionary<string, float>();
+	
+	void Start () {
+		rotator1 = GameObject.Find("Rotator 1");
+		rotators.Add(rotator1);
+		rotator2 = GameObject.Find("Rotator 2");
+		rotators.Add(rotator2);
+		rotator3 = GameObject.Find("Rotator 3");
+		rotators.Add(rotator3);
+
+		foreach(GameObject rotator in rotators){
+			rotatorSpeeds[rotator.name] = UnityEngine.Random.Range(-0.4f, 0.4f);
+		}
+	}
+
+	void Update() {
+		foreach(GameObject rotator in rotators){
+			rotator.transform.Rotate(new Vector3(0, 0, rotatorSpeeds[rotator.name]));
+		}
+	}
+}
--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -2,46 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraShake : MonoBehaviour {
+public class CameraShake : MonoBehaviour
+{
 
-	public Camera mainCam;
+    private Vector3 cameraStandardPos;
 
-	float shakeAmount = 0;
+    public Camera mainCam;
 
-	void Awake () {
-		if (mainCam == null) {
-			mainCam = Camera.main;
-		}
-	}
+    float shakeAmount = 0;
 
-	void Update(){
-		if( Input.GetKeyDown(KeyCode.T)){
-			Shake (0.1f, 0.2f);
-		}
-	}
+    void Awake()
+    {
+        if (mainCam == null)
+        {
+            mainCam = Camera.main;
+        }
+    }
 
-	public void Shake (float amount, float length) {
-		shakeAmount = amount;
-		InvokeRepeating("DoShake", 0, 0.01f);
-		Invoke("StopShake", length);
-	}
+    void Start()
+    {
+        if (mainCam != null)
+        {
+            cameraStandardPos = mainCam.transform.position;
+        }
+    }
 
-	void DoShake () {
-		if (shakeAmount > 0) {
-			Vector3 camPos = mainCam.transform.position;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Shake(0.1f, 0.2f);
+        }
+    }
 
-			float shakeAmtX = Random.value * shakeAmount * 2 - shakeAmount;
-			float shakeAmtY = Random.value * shakeAmount * 2 - shakeAmount;
+    public void Shake(float amount, float length)
+    {
+        shakeAmount = amount;
+        InvokeRepeating("DoShake", 0, 0.01f);
+        Invoke("StopShake", length);
+    }
 
-			camPos.x += shakeAmtX;
-			camPos.y += shakeAmtY;
+    void DoShake()
+    {
+        if (shakeAmount > 0)
+        {
+            Vector3 camPos = mainCam.transform.position;
 
-			mainCam.transform.position = camPos;
-		}
-	}
+            float shakeAmtX = Random.value * shakeAmount * 2 - shakeAmount;
+            float shakeAmtY = Random.value * shakeAmount * 2 - shakeAmount;
 
-	void StopShake () {
-		CancelInvoke("DoShake");
-		mainCam.transform.localPosition = new Vector3(0, -0.75f, -10);
-	}
+            camPos.x += shakeAmtX;
+            camPos.y += shakeAmtY;
+
+            mainCam.transform.position = camPos;
+        }
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("DoShake");
+        mainCam.transform.localPosition = cameraStandardPos;
+    }
 }
