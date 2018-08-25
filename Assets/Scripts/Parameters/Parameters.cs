06aebd93ab38a72f4abf7c26e319808451b20c37
Anton Bergman <antonbergman_@hotmail.com>
2018-08-25 16:10:38 +0200

    Fix some looks in main menu, change color of projectiles

--- Assets/Scripts/Parameters/Parameters.cs
+++ Assets/Scripts/Parameters/Parameters.cs
@@ -9,7 +9,7 @@ public static class Parameters {
 	public static float PROJECTILE_SPEED = 2.0f;
 	public static float PROJECTILE_RANGE = 0.5f;
 	public static float PROJECTILE_SCALE = 0.3f;
-	public static Color PROJECTILE_COLOR = Color.black;
+	public static Color PROJECTILE_COLOR = Color.red;
 	public static float ARENA_BOTTOM = -4.0f;
 
 	public static float SLOW_ATTACK_LIMIT = 2.5f;
