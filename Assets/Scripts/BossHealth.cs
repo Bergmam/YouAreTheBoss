24b4566599230158a2026d79198338da0a25373f
leigh <lagebergman@gmail.com>
2018-09-08 16:24:22 +0200

    Hide attack buttons when boss dies

--- Assets/Scripts/BossHealth.cs
+++ Assets/Scripts/BossHealth.cs
@@ -9,13 +9,16 @@ public class BossHealth : MonoBehaviour
     ProgressBarBehaviour bossHealthBar;
     private GameObject gameOverPanel;
     private GameObject scoreLabel;
+    private GameObject bossButtons;
 
     void Awake()
     {
         this.gameOverPanel = GameObject.Find("GameOverPanel");
         this.scoreLabel = GameObject.Find("ScoreLabel");
+        this.bossButtons = GameObject.Find("BossButtons");
         this.gameOverPanel.SetActive(false);
         this.scoreLabel.SetActive(true);
+        this.bossButtons.SetActive(true);
     }
 
     void Start()
@@ -40,14 +43,13 @@ public class BossHealth : MonoBehaviour
             }
             this.gameOverPanel.SetActive(true);
             this.scoreLabel.SetActive(false);
+            this.bossButtons.SetActive(false);
             ((WaveHandler)GameObject.FindObjectOfType(typeof(WaveHandler))).clearWave();
         }
     }
 
     public void playAgain()
     {
-        this.gameOverPanel.SetActive(false);
-        this.scoreLabel.SetActive(true);
         WaveNumber.waveNumber = 0;
         AttackLists.ResetUpgradedAttacks();
         SceneHandler.SwitchScene("Main Menu Scene");
