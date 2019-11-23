fafbee4205e3544991ea17c2deb83546d116c449
Anton Bergman <antonbergman_@hotmail.com>
2019-11-23 13:14:27 +0100

    Enable pausing and quiting the game during a round

--- Assets/Scripts/BossLookAt.cs
+++ Assets/Scripts/BossLookAt.cs
@@ -6,6 +6,11 @@ public class BossLookAt : MonoBehaviour {
 
 	void Update ()
 	{
+		if (PauseMenuController.gamePaused)
+		{
+			return;
+		}
+		
 		Vector3 touchPos = Vector3.zero;
 		if (Input.touchCount > 0)
 		{
---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    private GameObject pausePanel;
+    public static bool gamePaused;
+
+    void Awake() 
+    {
+        this.pausePanel = GameObject.Find("PausePanel");
+        gamePaused = false;
+    }
+
+    void Start()
+    {
+        this.pausePanel.SetActive(false);
+    }
+
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Escape)){
+            if (gamePaused)
+            {
+                this.UnpauseGame();
+            }
+            else
+            {
+                this.PauseGame();
+            }
+        }
+	}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void UnpauseGame()
+    {
+        gamePaused = false;
+        Time.timeScale = 1;
+        this.pausePanel.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        gamePaused = true;
+        Time.timeScale = 0;
+        this.pausePanel.SetActive(true);
+        //pausePanel.SetActive(true);
+        //Disable scripts that still work while timescale is set to 0
+    } 
+}
