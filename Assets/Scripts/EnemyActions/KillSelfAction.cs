c2fd4a1e46a0ff83f9e21deb087748a70be181ed
Lage Bergman <lagebergman@gmail.com>
2021-03-10 20:33:16 +0100

    Add an enemy action that kills the enemy itself when executed

---
+++ Assets/Scripts/EnemyActions/KillSelfAction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Action/Kill Self Action")]
+public class KillSelfAction : EnemyAction
+{
+    public override void Execute(GameObject enemyObject)
+    {
+        Destroy(enemyObject);
+    }
+}
