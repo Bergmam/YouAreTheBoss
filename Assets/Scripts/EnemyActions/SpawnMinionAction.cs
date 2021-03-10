42663a5dcddaa58903029c7f2e5b2c7201be4a4e
Lage Bergman <lagebergman@gmail.com>
2021-03-10 20:46:49 +0100

    Add an enemy action that spawns another unit when executed

---
+++ Assets/Scripts/EnemyActions/SpawnMinionAction.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy Action/Spawn Minion Action")]
+public class SpawnMinionAction : EnemyAction
+{
+    public EnemySettings Minon;
+
+    public override void Execute(GameObject enemyObject)
+    {
+        EnemySettings minion = Instantiate(this.Minon);
+        RadialPosition spawnPosition = RotationUtils.XYToRadialPos(enemyObject.transform.position);
+        Enemy enemy = enemyObject.GetComponent<Enemy>();
+        minion.predefinedPosition = true;
+        minion.spawnAngle = spawnPosition.GetAngle();
+        minion.spawnRadius = spawnPosition.GetRadius();
+        GameObject.FindObjectOfType<EnemySpawner>().InstantiateEnemyPrefab(minion);
+    }
+}
